826970f028d6c132544b4cc2b3fe10ac6a61cb78
eva.laurent.78@gmail.com <eva@mbp-de-eva.home>
2022-05-02 23:59:00 +0200

    push du projet

---
+++ lib/controller.rb
@@ -0,0 +1,30 @@
+
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+
+
+  get '/' do
+    erb :index
+  end
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    puts "c'est de la balle"
+    redirect '/'
+  end
+
+  get '/gossips/:id' do
+    erb :show, locals: {gossip: Gossip.find(params['id'])}
+   # puts  "Hello voici le potin n° #{params['id']}!"
+  end
+  
+
+
+
+
+end
